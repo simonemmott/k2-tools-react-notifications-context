cd4a677b1faf6973ad9010649195414130fcd0cf
simon <simon.emmott@yahoo.co.uk>
2022-08-09 08:45:27 +0100

    Ready for code cleanup and testing
    Testing working

---
+++ library-app/src/component/Notifications.test.js
@@ -0,0 +1,9 @@
+import Notifications from "./Notifications.js";
+
+describe("Notifications", () => {
+  
+  it("Has a Panel attribute", () => {
+    expect(Notifications.Panel).toBeTruthy();
+  })
+  
+});
